bdcb2262f55293c4f583fbf375a5aadaa9f2bfd6
TepSLore <55393334+TepSLore@users.noreply.github.com>
2020-11-10 14:34:36 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,55 +2,163 @@ import Card from './Card.js';
 import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
+import Creature from './Creature.js';
 
+// зачем @ почему
 // Отвечает является ли карта уткой.
 function isDuck(card) {
-    return card && card.quacks && card.swims;
+    return card.constructor === Duck;
 }
 
 // Отвечает является ли карта собакой.
 function isDog(card) {
-    return card instanceof Dog;
+    return card.constructor === Dog;
 }
-
-// Дает описание существа по схожести с утками и собаками
-function getCreatureDescription(card) {
-    if (isDuck(card) && isDog(card)) {
-        return 'Утка-Собака';
-    }
-    if (isDuck(card)) {
-        return 'Утка';
-    }
-    if (isDog(card)) {
-        return 'Собака';
-    }
-    return 'Существо';
-}
-
 
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Creature {
+    constructor() {
+        super("Мирная утка", 2)
+        this.currentPower = 2
+    }
+
+    getDescriptions() {
+        return [
+            'Утка',
+            ...super.getDescriptions()
+        ]
+    }
+
+    quacks () { console.log('quack'); }
+
+    swims () { console.log('float: both;'); }
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature {
+    constructor() {
+        super('Пес-бандит', 3)
+        this.currentPower = 3;
+        this.name = 'Пес-бандит';
+    }
+
+    getDescriptions() {
+        return [
+            'Псина',
+            ...super.getDescriptions()
+        ]
+    }
 }
 
+// Основа для Громилы.
+class Trasher extends Dog {
+    constructor() {
+        super('Громила', 5)
+        this.currentPower = 5;
+        this.name = 'Громила';
+    }
+
+    getDescriptions() {
+        return [
+            'Громила',
+            ...super.getDescriptions()
+        ]
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        this.view.signalAbility(() => { continuation(value - 1); });
+    }
+}
+
+class Gatling extends Creature {
+    constructor() {
+        super('Гатлинг', 6)
+    }
+
+    getDescriptions() {
+        return [
+            'Гатлинг',
+            ...super.getDescriptions()
+        ]
+    }
+
+    attack(gameContext, continuation) {
+        let cards = gameContext.oppositePlayer.table
+        const taskQueue = new TaskQueue();
+
+        for (let position = 0; position < cards.length; position++) {
+            taskQueue.push(onDone => {
+                const card = this.table[position]
+                if (card) {
+                    const gameContext = this.game.getContextForCard(position)
+                    card.attack(gameContext, continuation)
+                } else {
+                    onDone();
+                }
+            })
+        }
+
+        taskQueue.continueWith(continuation);
+    }
+}
+
+// Основа для Братков.
+class Lad extends Dog {
+    constructor() {
+        super('Браток', 2)
+        this.currentPower = 2;
+        this.name = 'Браток';
+    }
+
+    getDescriptions() {
+        return [
+            'Браток',
+            ...super.getDescriptions()
+        ]
+    }
+    
+    static getInGameCount() { return this.inGameCount || 0; }
+
+    static setInGameCount(value) { this.inGameCount = value; }
+
+    doAfterComingIntoPlay(gameContext, continuation) {
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        continuation();
+    };
+
+    doBeforeRemoving(continuation) {
+        
+    };
+
+    static getBonus() {
+
+    }
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation) {
+        
+    };
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        
+    }
+
+}
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
+    new Gatling(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Trasher(),
+    new Dog(),
+    new Dog(),
 ];
 
 
