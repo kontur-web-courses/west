8d31dac84e8d45c8ccfad03c2f6b2ef6d99aca17
Dank005 <daniil.yushkov2000@mail.ru>
2021-01-08 15:33:13 +0500

    Основное сделано

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,10 +1,16 @@
-const TaskQueue = function() {
+
+/*const TaskQueue = function() {
     function TaskQueue() {
+        this.tasks = [];
+        this.running = false;
+    }*/
+export default class TaskQueue {
+    constructor() {
         this.tasks = [];
         this.running = false;
     }
 
-    TaskQueue.prototype.push = function(run, dispose, duration) {
+    /*TaskQueue.prototype.push = function(run, dispose, duration) {
         if (duration === undefined || duration === null) {
             this.tasks.push({runAndContinue: run, dispose});
         } else {
@@ -19,37 +25,51 @@ const TaskQueue = function() {
             });
         }
         runNextTask(this);
-    };
-
-    TaskQueue.prototype.continueWith = function(action) {
-        this.push(action, null, 0);
-    };
-
-    function runNextTask(taskQueue) {
-        if (taskQueue.running || taskQueue.tasks.length === 0) {
-            return;
-        }
-        taskQueue.running = true;
-        const task = taskQueue.tasks.shift();
-
-        if (task.runAndContinue) {
-            setTimeout(() => {
-                task.runAndContinue(() => {
-                    task.dispose && task.dispose();
-                    taskQueue.running = false;
-
+    };*/
+    push(run, dispose, duration) {
+        if (duration === undefined || duration === null) {
+            this.tasks.push({runAndContinue: run, dispose});
+        } else {
+            this.tasks.push({
+                runAndContinue: (continuation) => {
+                    run();
                     setTimeout(() => {
-                        runNextTask(taskQueue);
-                    });
-                });
-            }, 0);
+                        continuation();
+                    }, duration);
+                },
+                dispose
+            });
         }
-        else {
-            runNextTask(taskQueue);
-        }
+        runNextTask(this);
     }
 
-    return TaskQueue;
-}();
+    /*TaskQueue.prototype.continueWith = function(action) {
+        this.push(action, null, 0);
+    };*/
+    continueWith(action) {
+        this.push(action, null, 0);
+    }
+}
+function runNextTask(taskQueue) {
+    if (taskQueue.running || taskQueue.tasks.length === 0) {
+        return;
+    }
+    taskQueue.running = true;
+    const task = taskQueue.tasks.shift();
 
-export default TaskQueue;
+    if (task.runAndContinue) {
+        setTimeout(() => {
+            task.runAndContinue(() => {
+                task.dispose && task.dispose();
+                taskQueue.running = false;
+
+                setTimeout(() => {
+                    runNextTask(taskQueue);
+                });
+            });
+        }, 0);
+    }
+    else {
+        runNextTask(taskQueue);
+    }
+}
