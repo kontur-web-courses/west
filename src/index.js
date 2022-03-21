50ddf50dcfde788938427b9de4691374f76ba3ce
Алёночка Савельева <savelyeva.alena@ya.ru>
2022-03-21 13:51:11 +0500

    second task

--- src/index.js
+++ src/index.js
@@ -3,9 +3,26 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
+
+class Duck extends Card{
+    constructor(name = 'Мирная утка',maxPower = 2, image = 'sheriff.png') {
+        super(name, maxPower, image);
+    };
+
+    quacks = function () { console.log('quack') };
+    swims = function () { console.log('float: both;') };
+}
+
+class Dog extends Card{
+    constructor(name = 'Пес-бандит', maxPower = 3, image = 'bandit.png') {
+        super(name, maxPower, image);
+    };
+}
+
+
 // Отвечает является ли карта уткой.
 function isDuck(card) {
-    return card && card.quacks && card.swims;
+    return card instanceof Duck;
 }
 
 // Отвечает является ли карта собакой.
@@ -18,39 +35,26 @@ function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
         return 'Утка-Собака';
     }
-    if (isDuck(card)) {
+    else if (isDuck(card)) {
         return 'Утка';
     }
-    if (isDog(card)) {
+    else if (isDog(card)) {
         return 'Собака';
     }
     return 'Существо';
 }
 
 
-
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
-}
-
-
-// Основа для собаки.
-function Dog() {
-}
-
-
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
