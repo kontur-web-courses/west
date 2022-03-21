0fb365fc089fe769c479978d3d460993c99221ca
NoSteam1Bazeev <damir240402@mail.ru>
2022-03-21 14:53:30 +0500

    Lad

--- src/index.js
+++ src/index.js
@@ -16,6 +16,41 @@ class Trasher extends Dog {
     }
 }
 
+class Lad extends Dog{
+    constructor(name = 'Браток', power = 2) {
+        super(name, power);
+    }
+
+    static getInGameCount() { return this.inGameCount || 0; }
+    static setInGameCount(value) { this.inGameCount = value; }
+    static getBonus() {
+        return this.getInGameCount() * (this.getInGameCount() + 1) / 2;
+    }
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation) {
+        this.view.signalAbility(() => {
+            continuation(value + Lad.getBonus());
+        });
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        this.view.signalAbility(() => {
+            continuation(Math.max(value - Lad.getBonus(), 0));
+        });
+    }
+
+    doAfterComingIntoPlay(gameContext, continuation) {
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        Lad.setInGameCount(Lad.getInGameCount() + 1);
+        continuation();
+    }
+
+    doBeforeRemoving(continuation) {
+        Lad.setInGameCount(Lad.getInGameCount() - 1);
+        continuation();
+    }
+
+}
 
 const seriffStartDeck = [
     new Duck(),
