be338465e152c95c218d3cdd96b6aefcbcddadd4
MatveyMatveyIl <matvey.ilichev@gmail.com>
2022-03-21 11:55:12 +0500

    2.1

--- src/index.js
+++ src/index.js
@@ -47,14 +47,12 @@ class Dog extends Card{
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(), new Duck(), new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
