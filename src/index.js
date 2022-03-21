a3658a0f2bd5a20ff1c066b7239589e3cd24f8f8
artem <2001raf@gmail.com>
2022-03-21 12:34:37 +0500

    TASK 2 & 3 DONE

--- src/index.js
+++ src/index.js
@@ -27,30 +27,37 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
-
-
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Creature extends Card {
+    getDescriptions() { return [super.getDescriptions(), getCreatureDescription(this)] };
 }
 
+// Основа для утки.
+class Duck extends Creature {
+    constructor() {
+        super('Мирная утка', 2);
+    }
+    quacks() { console.log('quack') };
+    swims() { console.log('float: both;') };
+}
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature {
+    constructor() {
+        super('Пес-бандит', 3);
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
