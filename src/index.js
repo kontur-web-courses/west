4d8ee42a24e6e382cd320edeb6d676a4362912fa
Artemii Kurganov <aokurganov@mail.ru>
2022-03-22 22:24:29 +0500

    Change index.js 1) Formatting 2) Add default values to constructor 3) Fix Gatling

--- src/index.js
+++ src/index.js
@@ -27,31 +27,31 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
-class Creature extends Card{
+class Creature extends Card {
     constructor(name, health) {
         super(name, health);
     }
 
-    getDescriptions(){
+    getDescriptions() {
         return [getCreatureDescription(this), super.getDescriptions()]
     }
 }
 
-class Gatling extends Creature{
-    constructor() {
-        super('Гатлинг', 6);
+class Gatling extends Creature {
+    constructor(name='Гатлинг', health=6) {
+        super(name, health);
     };
 
-    attack(gameContext) {
+    attack(gameContext, continuation) {
         const taskQueue = new TaskQueue();
         const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
 
         taskQueue.push(onDone => this.view.showAttack(onDone));
         taskQueue.push(onDone => {
             const oppositeCardList = oppositePlayer.table;
-            for (let oppositeCard of oppositeCardList){
+            for (let oppositeCard of oppositeCardList) {
                 if (oppositeCard) {
-                    this.dealDamageToCreature(this.currentPower, oppositeCardList, gameContext, onDone);
+                    this.dealDamageToCreature(this.currentPower, oppositeCard, gameContext, onDone);
                 } else {
                     this.dealDamageToPlayer(1, gameContext, onDone);
                 }
@@ -63,41 +63,45 @@ class Gatling extends Creature{
 }
 
 class Duck extends Creature {
-    constructor() {
-        super('Мирная утка', 2);
+    constructor(name = 'Мирная утка', health = 2) {
+        super(name, health);
     }
 
-    quacks() { console.log('quack') };
-    swims() { console.log('float: both;') };
+    quacks() {
+        console.log('quack')
+    };
+
+    swims() {
+        console.log('float: both;')
+    };
 }
 
 
 class Dog extends Creature {
-    constructor(name='Пес-бандит', power=3) {
+    constructor(name = 'Пес-бандит', power = 3) {
         super(name, power);
     }
 }
 
-class Trasher extends Dog{
-    constructor() {
-        super('Громила', 5);
+class Trasher extends Dog {
+    constructor(name = 'Громила', health = 5) {
+        super(name, health);
     }
+
     modifyTakenDamage(value, fromCard, gameContext, continuation) {
         super.modifyTakenDamage(value - 1, fromCard, gameContext, continuation);
     }
 }
 
 const seriffStartDeck = [
+    new Duck(),
     new Gatling(),
-    new Duck(),
-    new Duck(),
     new Duck(),
 ];
 const banditStartDeck = [
     new Dog(),
+    new Trasher(),
     new Dog(),
-    new Dog(),
-
 ];
 
 
