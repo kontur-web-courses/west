31e29f4e15a94f457883964e176d1d2930029d18
art3xa <A.Kh.Romanov@urfu.me>
2023-03-20 14:28:19 +0500

    done 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-import Card from './Card.js';
-import Game from './Game.js';
-import TaskQueue from './TaskQueue.js';
-import SpeedRate from './SpeedRate.js';
+import Card from "./Card.js";
+import Game from "./Game.js";
+import TaskQueue from "./TaskQueue.js";
+import SpeedRate from "./SpeedRate.js";
+
+
+class Creature extends Card {
+    constructor(name, maxPower, image) {
+        super(name, maxPower, image);
+    }
+
+    getDescriptions() {
+        return [
+            getCreatureDescription(this),
+            ...super.getDescriptions()
+        ];
+    }
+}
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
@@ -16,33 +30,35 @@ function isDog(card) {
 // Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
-        return 'Утка-Собака';
+        return "Утка-Собака";
     }
     if (isDuck(card)) {
-        return 'Утка';
+        return "Утка";
     }
     if (isDog(card)) {
-        return 'Собака';
+        return "Собака";
     }
-    return 'Существо';
+    return "Существо";
 }
 
 
-class Duck extends Card {
+class Duck extends Creature {
     constructor() {
-        super('Мирная утка', 2, null);
+        super("Мирная утка", 2, null);
     }
+
     quacks() {
-        console.log('quack')
+        console.log("quack");
     };
+
     swims() {
-        console.log('float: both;')
+        console.log("float: both;");
     };
 }
 
-class Dog extends Card {
+class Dog extends Creature {
     constructor() {
-        super('Пес-бандит', 3, null);
+        super("Пес-бандит", 3, null);
     }
 }
 
@@ -67,5 +83,5 @@ SpeedRate.set(1);
 
 // Запуск игры.
 game.play(false, (winner) => {
-    alert('Победил ' + winner.name);
+    alert("Победил " + winner.name);
 });
