e8fcec1e2f55e3a00d3481b47b3f6cef291af7e9
Lada Kuzmina <lada.kuzmina2003@gmail.com>
2023-03-20 15:07:17 +0500

    2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import SpeedRate from './SpeedRate.js';
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
-    return card && card.quacks && card.swims;
+    return card instanceof Duck;
 }
 
 // Отвечает является ли карта собакой.
@@ -28,29 +28,35 @@ function getCreatureDescription(card) {
 }
 
 
-
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Card{
+    constructor(...rest) {
+        super(rest);
+        this.name = "Мирная утка";
+        this.maxPower = 2;
+    }
+    quacks = function () { console.log('quack') };
+    swims = function () { console.log('float: both;') };
 }
 
-
-// Основа для собаки.
-function Dog() {
+class Dog extends Card{
+    constructor(...rest) {
+        super(rest);
+        this.name = "Пес-бандит";
+        this.maxPower = 3;
+    }
 }
-
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
+
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
