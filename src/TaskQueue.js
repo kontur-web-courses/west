d39c29e9864744ce32d417cfdbca3ae2a33af510
AndreyKuklinov <Andreydolphin@mail.ru>
2023-03-20 17:35:09 +0500

    Переписал TaskQueue на класс

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,10 +1,10 @@
-const TaskQueue = function() {
-    function TaskQueue() {
+class TaskQueue {
+    constructor() {
         this.tasks = [];
         this.running = false;
     }
 
-    TaskQueue.prototype.push = function(run, dispose, duration) {
+    push(run, dispose, duration) {
         if (duration === undefined || duration === null) {
             this.tasks.push({runAndContinue: run, dispose});
         } else {
@@ -19,13 +19,13 @@ const TaskQueue = function() {
             });
         }
         runNextTask(this);
-    };
+    }
 
-    TaskQueue.prototype.continueWith = function(action) {
+    continueWith(action) {
         this.push(action, null, 0);
-    };
+    }
 
-    function runNextTask(taskQueue) {
+    runNextTask(taskQueue) {
         if (taskQueue.running || taskQueue.tasks.length === 0) {
             return;
         }
@@ -48,8 +48,62 @@ const TaskQueue = function() {
             runNextTask(taskQueue);
         }
     }
+}
 
-    return TaskQueue;
-}();
+// const TaskQueue = function() {
+//     function TaskQueue() {
+//         this.tasks = [];
+//         this.running = false;
+//     }
+
+//     TaskQueue.prototype.push = function(run, dispose, duration) {
+//         if (duration === undefined || duration === null) {
+//             this.tasks.push({runAndContinue: run, dispose});
+//         } else {
+//             this.tasks.push({
+//                 runAndContinue: (continuation) => {
+//                     run();
+//                     setTimeout(() => {
+//                         continuation();
+//                     }, duration);
+//                 },
+//                 dispose
+//             });
+//         }
+//         runNextTask(this);
+//     };
+
+//     TaskQueue.prototype.continueWith = function(action) {
+//         this.push(action, null, 0);
+//     };
+
+//     function runNextTask(taskQueue) {
+//         if (taskQueue.running || taskQueue.tasks.length === 0) {
+//             return;
+//         }
+//         taskQueue.running = true;
+//         const task = taskQueue.tasks.shift();
+
+//         if (task.runAndContinue) {
+//             setTimeout(() => {
+//                 task.runAndContinue(() => {
+//                     task.dispose && task.dispose();
+//                     taskQueue.running = false;
+
+//                     setTimeout(() => {
+//                         runNextTask(taskQueue);
+//                     });
+//                 });
+//             }, 0);
+//         }
+//         else {
+//             runNextTask(taskQueue);
+//         }
+//     }
+
+//     return TaskQueue;
+// }();
 
 export default TaskQueue;
+
+
