dbec6cdd33d080841b390386b8ac0eb95c12a84c
Pavel Reshetnikov <pasha.keyzet@yandex.ru>
2023-03-20 18:56:20 +0500

    Lads

--- src/index.js
+++ src/index.js
@@ -35,13 +35,55 @@ class Dog extends Creature {
 }
 
 class Trasher extends Dog {
-    constructor(name) {
-        super(name, 5);
+    constructor(name = "Громила", power = 5) {
+        super(name, power);
     }
     
     modifyTakenDamage = function (value, fromCard, gameContext, continuation) {
         this.view.signalAbility(() => continuation(value - 1));
     };
+}
+
+class Lad extends Dog {
+    constructor(name = "Браток", power = 2) {
+        super(name, power);
+    }
+
+    static inGameCount;
+    static getInGameCount() { return this.inGameCount || 0; }
+    static setInGameCount(value) { this.inGameCount = value; }
+
+    doAfterComingIntoPlay(gameContext, continuation) {
+        const ladsCount = Lad.getInGameCount();
+        Lad.setInGameCount(ladsCount + 1);
+        continuation();
+    };
+
+    doBeforeRemoving(continuation) {
+        const ladsCount = Lad.getInGameCount();
+        Lad.setInGameCount(ladsCount - 1);
+        continuation();
+    }
+
+    static getBonus() { 
+        return this.getInGameCount() * (this.getInGameCount() + 1) / 2;
+    }
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation) {
+        continuation(value + Lad.getBonus());
+    };
+0
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        continuation(value - Lad.getBonus());
+    };
+
+    getDescriptions(){
+        return [this.getLadDescription(), super.getDescriptions()];
+    }
+
+    getLadDescription() {
+        return "Чем их больше, тем они сильнее";
+    }
 }
 
 // Отвечает является ли карта уткой.
@@ -53,6 +95,10 @@ function isDuck(card) {
 // Отвечает является ли карта собакой.
 function isDog(card) {
     return card instanceof Dog;
+}
+
+function isLad(card) {
+    return card instanceof Lad;
 }
 
 // Дает описание существа по схожести с утками и собаками
@@ -90,7 +136,8 @@ const seriffStartDeck = [
     new Duck("", 2),
 ];
 const banditStartDeck = [
-    new Trasher("Joe"),
+    new Lad(),
+    new Lad(),
 ];
 
 // Создание игры.
