10c7a65757ba67a05ad493392df3d58b8c8e3dae
Евгений <evgeniisergeev2002@list.ru>
2023-03-20 15:45:40 +0500

    add pseudoduck

--- src/index.js
+++ src/index.js
@@ -96,14 +96,28 @@ class Gatling extends Creature {
 }
 
 
+class PseudoDuck extends Dog {
+    constructor() {
+        super("Псевдоутка", 3, "pseudoDuck.png");
+    }
+
+    quacks() {
+        console.log('quack')
+    }
+
+    swims() {
+        console.log('float: both;')
+    }
+}
+
 const seriffStartDeck = [
     new Duck(),
     new Duck(),
-    new Gatling(),
+
 ];
 const banditStartDeck = [
-    new Trasher(),
     new Dog(),
+    new PseudoDuck(),
     new Dog(),
 ];
 
