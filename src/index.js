6a65750652782452f3b3187c4140995b897338bb
bolthen <92172378+bolthen@users.noreply.github.com>
2023-03-20 14:50:12 +0500

    Add Duck and Dog Cards

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import SpeedRate from './SpeedRate.js';
 
 // Отвечает является ли карта уткой.
 function isDuck(card) {
-    return card && card.quacks && card.swims;
+    return card instanceof Duck;
 }
 
 // Отвечает является ли карта собакой.
@@ -28,29 +28,39 @@ function getCreatureDescription(card) {
 }
 
 
+class Duck extends Card {
+    constructor(image) {
+        super("Мирная утка", 2, image);
+    }
 
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+    static quacks() {
+        console.log('quack');
+    }
+
+    static swims() {
+        console.log('float: both;');
+    }
+
 }
 
+class Dog extends Card{
+    constructor(image) {
+        super("Пес-бандит", 3, image);
+    }
 
-// Основа для собаки.
-function Dog() {
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
