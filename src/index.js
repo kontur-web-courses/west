daf0acc0730d79c8ce0314b736af76839be25109
Ilya <sckremyulock@gmail.com>
2023-03-20 17:45:51 +0500

    Task2

--- src/index.js
+++ src/index.js
@@ -26,31 +26,57 @@ function getCreatureDescription(card) {
     }
     return 'Существо';
 }
+class Duck extends Card {
+    constructor(name = 'Мирная утка', maxPower = 2) {
+        super(name, maxPower);
+    }
 
+    quacks() {
+        console.log('quack');
+    }
 
+    swims() {
+        console.log('float: both;');
+    }
+}
+
+class Dog extends Card {
+    constructor(name = 'Пес-бандит', maxPower = 3) {
+        super(name, maxPower);
+    }
+}
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
-}
+// function Duck() {
+//     this.quacks = function () { console.log('quack') };
+//     this.swims = function () { console.log('float: both;') };
+// }
 
 
 // Основа для собаки.
-function Dog() {
-}
+// function Dog() {
+// }
 
 
 // Колода Шерифа, нижнего игрока.
+// const seriffStartDeck = [
+//
+//     new Card('Мирный житель', 2),
+//     new Card('Мирный житель', 2),
+//     new Card('Мирный житель', 2),
+// ];
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
+// const banditStartDeck = [
+//     new Card('Бандит', 3),
+// ];
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
@@ -64,3 +90,7 @@ SpeedRate.set(1);
 game.play(false, (winner) => {
     alert('Победил ' + winner.name);
 });
+
+
+
+
