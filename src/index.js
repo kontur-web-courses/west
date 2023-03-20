b38e1e7c1ad6092b70f625ab00a301ecf2a52e27
TashK101 <Natalia.Karimova@urfu.me>
2023-03-20 15:20:35 +0500

    Trasher start

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
+class Trasher extends Dog{
+    constructor(){
+        super("Громила", 5);
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        this.view.signalAbility(() => continuation(value - 1));
+    }
+
+    getDescribtions
+}
+
 // Отвечает является ли карта уткой.
 function isDuck(card) {
     return card && card.quacks && card.swims;
