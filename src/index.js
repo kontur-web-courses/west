7984f6e3ab728e978bae321d279946ce33a37a36
Dengoan969 <dengoan969@gmail.com>
2023-03-20 14:12:37 +0500

    2

--- src/index.js
+++ src/index.js
@@ -27,30 +27,37 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+class Duck extends Card{
+    constructor() {
+        super('Мирная утка', 2);
+    }
 
+    quacks() {
+        console.log('quack')
+    }
 
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+    swims () {
+        console.log('float: both;')
+    }
 }
 
-
-// Основа для собаки.
-function Dog() {
+class Dog extends Card {
+    constructor() {
+        super('Пес-бандит', 3);
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck()
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog()
 ];
 
 
