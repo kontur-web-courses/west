c94f2d0d9423f6da6bbd91ac4580d9046d0c8041
antoshkaxxr <63500718+antoshkaxxr@users.noreply.github.com>
2024-03-19 17:37:08 +0500

    tasks 3-5

--- src/index.js
+++ src/index.js
@@ -27,10 +27,20 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+// Задание 3
+class Creature extends Card {
+    getDescriptions() {
+        return [getCreatureDescription(this), ...super.getDescriptions()];
+    }
+}
+
 // Задание 2
-class Duck extends Card {
+class Duck extends Creature {
     constructor() {
-        super('Мирная утка', 2);
+        super();
+        this.name = 'Мирная утка';
+        this.maxPower = 2;
+        this.currentPower = 2;
     }
 
     quacks() {
@@ -43,9 +53,59 @@ class Duck extends Card {
 }
 
 
-class Dog extends Card {
+class Dog extends Creature {
     constructor() {
-        super('Пес-бандит', 3);
+        super();
+        this.name = 'Пес-бандит';
+        this.maxPower = 3;
+        this.currentPower = 3;
+    }
+}
+
+// Задание 4
+class Trasher extends Dog {
+    constructor() {
+        super();
+        this.name = 'Громила';
+        this.maxPower = 5;
+        this.currentPower = 5;
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        const reducedDamage = value - 1;
+        this.view.signalAbility(() => {
+            continuation(reducedDamage < 0 ? 0 : reducedDamage);
+        });
+    }
+
+    getDescriptions() {
+        const baseDescriptions = super.getDescriptions();
+        const description = 'Получает на 1 меньше урона при атаке';
+        return [description, ...baseDescriptions];
+    }
+}
+
+class Gatling extends Creature {
+    constructor() {
+        super();
+        this.name = 'Гатлинг';
+        this.maxPower = 6;
+        this.currentPower = 6;
+    }
+
+    attack(gameContext, continuation) {
+        const taskQueue = new TaskQueue();
+
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+        for (const oppositeCard of oppositePlayer.table) {
+            taskQueue.push(onDone => {
+                this.dealDamageToCreature(this.currentPower, oppositeCard, gameContext, onDone);
+            });
+        }
+
+        taskQueue.continueWith(continuation);
     }
 }
 
@@ -55,10 +115,13 @@ const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
+    new Gatling(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
+    new Trasher(),
+    new Dog(),
     new Dog(),
 ];
 
