9970f0151fd0c68466da019fd23cc7cdc280e581
L0veR1ck <lavr.rar@yandex.ru>
2024-03-19 19:18:20 +0500

    task 5 done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import Game from './Game.js';
 import SpeedRate from './SpeedRate.js';
 import Creature from "./Creature.js";
+import TaskQueue from "./TaskQueue.js";
 
 // Отвечает, является ли карта уткой.
 function isDuck(card) {
@@ -66,24 +67,44 @@ class Trasher extends Dog {
     }
 }
 
+class Gatling extends Creature {
+    constructor(name = 'Гатлинг', maxPower = 6, image = null) {
+        super(name, maxPower, image);
+        this.damage = 2;
+    }
+
+    attack(gameContext, continuation) {
+        const taskQueue = new TaskQueue();
+
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+        for (const oppositeCard of gameContext.oppositePlayer.table.filter(card => card)) {
+            taskQueue.push(onDone => this.dealDamageToCreature(this.damage, oppositeCard, gameContext, onDone));
+        }
+
+        taskQueue.continueWith(continuation);
+    }
+}
+
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
-    new Duck(),
+    new Gatling(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
     new Trasher(),
+    new Dog(),
+    new Dog(),
 ];
 
 // Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
 
 // Глобальный объект, позволяющий управлять скоростью всех анимаций.
-SpeedRate.set(1);
+SpeedRate.set(3);
 
 // Запуск игры.
 game.play(false, (winner) => {
