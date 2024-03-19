16d360019c74f17dc5646b4c976ca141fb92f348
mgmman <115487609+mgmman@users.noreply.github.com>
2024-03-19 18:10:43 +0500

    added thrasher and gatling (4-5)

--- src/index.js
+++ src/index.js
@@ -61,17 +61,54 @@ class Dog extends Creature{
     };
 }
 
+class Trasher extends Dog{
+    constructor() {
+        super('Громила', 5);
+    };
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        this.view.signalAbility(() => {
+            super.modifyTakenDamage(value - 1, fromCard, gameContext, continuation);
+        })
+    };
+
+    getDescriptions() {
+        return ['Получает на 1 меньше урона', ...super.getDescriptions()]
+    };
+}
+
+class Gatling extends Creature {
+    constructor() {
+        super('Гатлинг', 6);
+    }
+    attack(gameContext, continuation) {
+        const taskQueue = new TaskQueue();
+
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+        for (const oppositeCard of game.oppositePlayer.table) {
+            taskQueue.push(onDone => {
+                this.dealDamageToCreature(this.currentPower, oppositeCard, gameContext, onDone);
+            });
+        }
+
+        taskQueue.continueWith(continuation);
+    };
+}
+
 
 // Колода Шерифа, нижнего игрока.
         const seriffStartDeck = [
+            //new Duck(),
             new Duck(),
-            new Duck(),
+            new Gatling(),
             new Duck(),
         ];
 
 // Колода Бандита, верхнего игрока.
         const banditStartDeck = [
-            new Dog(),
+            //new Dog(),
+            new Trasher(),
+            new Trasher()
         ];
 
 
