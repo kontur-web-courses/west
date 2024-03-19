4d91a2cabe851c04e355d727062c63131ecbf6e8
ISich <arseny.voroni@yandex.ru>
2024-03-19 19:08:46 +0500

    ex 5

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ function getCreatureDescription(card) {
 class Creature extends Card {
     getDescriptions() {
         let result = [getCreatureDescription(this)]
-        return [getCreatureDescription(this), super.getDescriptions()];
+        return [getCreatureDescription(this), ...super.getDescriptions()];
     }
 }
 
@@ -60,11 +60,27 @@ class Trasher extends Dog{
     constructor(name='Громила', maxPower=5){
         super(name, maxPower);
     }
-    modifyTakenDamage(v){
-        super.modifyTakenDamage(v-1);
+    modifyTakenDamage(value, fromCard, gameContext, continuation){
+        this.view.signalAbility(() => super.modifyTakenDamage(value-1, fromCard, gameContext, continuation));
     }
-    
+    getDescriptions(){
+        return ['Он получает на 1 меньше урона', ...super.getDescriptions()];
+    }
     //this.view.signalAbility(() => { // то, что надо сделать сразу после мигания. }
+}
+
+class Gatling extends Creature{
+    constructor(name='Гатлинг', maxPower=6){
+        super(name, maxPower);
+    }
+    attack(gameContext, continuation){
+        const taskQueue = new TaskQueue();
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+        for (const enemy of game.oppositePlayer.table) {
+            taskQueue.push(onDone => {this.dealDamageToCreature(this.currentPower, enemy, gameContext, onDone);});
+        }
+        taskQueue.continueWith(continuation);
+    };
 }
 
 // Колода Шерифа, нижнего игрока.
@@ -72,11 +88,14 @@ const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
+    new Gatling(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
     new Trasher(),
+    new Dog(),
+    new Dog(),
 ];
 //console.log(isDuck(seriffStartDeck[0]))
 
