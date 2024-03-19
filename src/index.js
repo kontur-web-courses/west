78ea77c963436be1be56125452998b172626c0be
mgmman <115487609+mgmman@users.noreply.github.com>
2024-03-19 17:58:40 +0500

    added class heirarchy (2, 3)

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
-// Отвечает является ли карта уткой.
+
 function isDuck(card) {
     return card && card.quacks && card.swims;
 }
 
-// Отвечает является ли карта собакой.
 function isDog(card) {
     return card instanceof Dog;
 }
@@ -27,31 +26,53 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+class Creature extends Card {
+    constructor(...props) {
+        super(...props);
+    }
+
+    getDescriptions() {
+        return [getCreatureDescription(this), ...super.getDescriptions()];
+    }
+}
+
 
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Creature{
+    constructor(name = 'Мирная утка', power = 2) {
+        super(name, power);
+    };
+
+    quacks() {
+        console.log('quack');
+    };
+
+    swims() {
+        console.log('float: both;');
+    };
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature{
+    constructor(name = 'Пес-бандит', power = 3) {
+        super(name, power);
+    };
 }
 
 
 // Колода Шерифа, нижнего игрока.
-const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-];
+        const seriffStartDeck = [
+            new Duck(),
+            new Duck(),
+            new Duck(),
+        ];
 
 // Колода Бандита, верхнего игрока.
-const banditStartDeck = [
-    new Card('Бандит', 3),
-];
+        const banditStartDeck = [
+            new Dog(),
+        ];
 
 
 // Создание игры.
