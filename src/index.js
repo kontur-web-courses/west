cd1fb4def27aedf43e56e2ccd58eb0e5acc95dca
Vearsmon <vearsmon285537@gmail.com>
2024-03-19 17:56:19 +0500

    task 2

--- src/index.js
+++ src/index.js
@@ -30,27 +30,36 @@ function getCreatureDescription(card) {
 
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Card {
+    constructor() {
+        super('Мирная утка', 2);
+    }
 }
+
+Duck.prototype.quacks = function () { console.log('quack') };
+Duck.prototype.swims = function () { console.log('float: both;') };
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Card {
+    constructor() {
+        super('Пес-бандит', 3);
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck()
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
+    new Dog(),
+    new Dog()
 ];
 
 
