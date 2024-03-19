e90e18813cf583b456620d1e91156636497fa83c
DemitrySennikov <demitry.sennikov.061103@mail.ru>
2024-03-19 19:29:38 +0500

    Fixed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
+
+
+
 // Отвечает является ли карта уткой.
 function isDuck(card) {
     return card && card.quacks && card.swims;
@@ -27,20 +30,112 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+class Creature extends Card {
+    constructor(name, maxPower) {
+        super(name, maxPower);
+    }
 
+    getDescriptions() {
+        return [
+            getCreatureDescription(this),
+            ...super.getDescriptions()
+        ];
+    }
+}
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Creature{
+    constructor(name = 'Мирная утка', maxPower = 2) {
+        super(name, maxPower);
+    }
+
+    quacks() { console.log('quack') };
+    swims() { console.log('float: both;') };
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature {
+    constructor(name = 'Пёс-бандит', maxPower = 3) {
+        super(name, maxPower);
+    }
+}
+
+class Trasher extends Dog {
+    constructor() {
+        super('Громила', 5);
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        this.view.signalAbility(() => {super.modifyTakenDamage(value-1, fromCard, gameContext, continuation)});
+    }
+}
+
+class Lad extends Dog {
+    constructor() {
+        super('Браток', 2);
+    }
+
+    static getInGameCount() { 
+        return this.inGameCount || 0; 
+    } 
+    
+    static setInGameCount(value) { 
+        this.inGameCount = value; 
+    }
+
+    static getBonus () {
+        const count = this.getInGameCount();
+        return count * (count + 1) / 2;
+    }
+
+    doAfterComingIntoPlay(gameContext, continuation) {
+        Lad.setInGameCount(Lad.getInGameCount() + 1);
+        super.doAfterComingIntoPlay(gameContext, continuation);
+    }
+
+    doBeforeRemoving(continuation) {
+        Lad.setInGameCount(Lad.getInGameCount() - 1);
+        super.doBeforeRemoving(continuation);
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        super.modifyTakenDamage(value - Lad.getBonus(), fromCard, gameContext, continuation);
+    }
+
+    modifyDealedDamageToCreature (value, toCard, gameContext, continuation) {
+        super.modifyDealedDamageToCreature(value + Lad.getBonus(), toCard, gameContext, continuation);
+    }
+
+    getDescriptions() {
+        const description = super.getDescriptions();
+        if (Lad.prototype.hasOwnProperty('modifyDealedDamageToCreature') ||
+        Lad.prototype.hasOwnProperty('modifyTakenDamage')) {
+            description.push("Чем их больше, тем они сильнее");
+        }
+        return description;
+    }
+}
+
+class Gatling extends Creature {
+    constructor() {
+        super('Гатлинг', 6);
+    }
+
+    attack(gameContext, continuation) {
+        const queue = new TaskQueue();
+        const table = gameContext.oppositePlayer.table;
+        for (let card of table) {
+            queue.push(onDone => this.view.showAttack(onDone));
+            queue.push(onDone => this.dealDamageToCreature(2, card, gameContext, onDone));
+        }
+        queue.continueWith(continuation);
+    }
 }
 
 
+
+/*
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
     new Card('Мирный житель', 2),
@@ -51,6 +146,40 @@ const seriffStartDeck = [
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
     new Card('Бандит', 3),
+];
+
+
+const seriffStartDeck = [
+    new Duck(),
+    new Duck(),
+    new Duck(),
+    new Duck(),
+];
+const banditStartDeck = [
+    new Trasher(),
+];
+
+const seriffStartDeck = [
+    new Duck(),
+    new Duck(),
+    new Duck(),
+    new Gatling(),
+];
+const banditStartDeck = [
+    new Trasher(),
+    new Dog(),
+    new Dog(),
+];
+*/
+
+const seriffStartDeck = [
+    new Duck(),
+    new Duck(),
+    new Duck(),
+];
+const banditStartDeck = [
+    new Lad(),
+    new Lad(),
 ];
 
 
