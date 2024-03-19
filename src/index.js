b7147d00716fc49e4ede460c5b97df9508b338e9
HairySnake <mameva83@outlook.com>
2024-03-19 23:22:55 +0500

    added duck and dog

--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
-// Отвечает является ли карта уткой.
-function isDuck(card) {
-    return card && card.quacks && card.swims;
-}
 
-// Отвечает является ли карта собакой.
-function isDog(card) {
-    return card instanceof Dog;
-}
 
-// Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
         return 'Утка-Собака';
@@ -28,29 +19,14 @@ function getCreatureDescription(card) {
 }
 
 
-
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
-}
-
-
-// Основа для собаки.
-function Dog() {
-}
-
-
-// Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
-// Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
@@ -69,10 +45,28 @@ class Duck extends Card {
     constructor() {
         super("Мирная утка", 2)
     }
+
+    quacks() { 
+        console.log('quack'); 
+    }
+
+    swims() { 
+        console.log('float: both;'); 
+    }
+
+
 }
 
 class Dog extends Card {
     constructor() {
         super("Пес-бандит", 3)
     }
+}
+
+function isDuck(card) {
+    return card && card.quacks && card.swims;
+}
+
+function isDog(card) {
+    return card instanceof Dog;
 }
