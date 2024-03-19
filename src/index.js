e8e1651a01910d9d39137fffb1647b87cbbee0e0
lordwartz <Lwartz@yandex.ru>
2024-03-19 18:10:49 +0500

    add duck

--- src/index.js
+++ src/index.js
@@ -30,29 +30,29 @@ function getCreatureDescription(card) {
 
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Card{
+    constructor(name = "Мирная утка", power = 2) {
+        super(name, power);
+    }
+    quacks = function () { console.log('quack') };
+    swims = function () { console.log('float: both;') };
 }
 
+Object.setPrototypeOf(Duck, Card);
 
 // Основа для собаки.
 function Dog() {
 }
 
 
-// Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
-
-// Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
-
 
 // Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
