5a01b21ee175062bfda1b71b6873b2df52c265a6
magnus5552 <isackov.stepa@yandex.ru>
2024-03-19 17:25:43 +0500

    TaskQueue to class

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,10 +1,10 @@
-const TaskQueue = function() {
-    function TaskQueue() {
+export default class TaskQueue {
+    constructor() {
         this.tasks = [];
         this.running = false;
     }
 
-    TaskQueue.prototype.push = function(run, dispose, duration) {
+    push(run, dispose, duration) {
         if (duration === undefined || duration === null) {
             this.tasks.push({runAndContinue: run, dispose});
         } else {
@@ -19,13 +19,13 @@ const TaskQueue = function() {
             });
         }
         runNextTask(this);
-    };
+    }
 
-    TaskQueue.prototype.continueWith = function(action) {
+    continueWith(action) {
         this.push(action, null, 0);
-    };
+    }
 
-    function runNextTask(taskQueue) {
+    runNextTask(taskQueue) {
         if (taskQueue.running || taskQueue.tasks.length === 0) {
             return;
         }
@@ -48,8 +48,4 @@ const TaskQueue = function() {
             runNextTask(taskQueue);
         }
     }
-
-    return TaskQueue;
-}();
-
-export default TaskQueue;
+}
