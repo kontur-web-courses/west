a2a16b63726d64c3914a2c15b2eb65f9eac75a22
Mike <kononov.mixa2045@gmail.com>
2024-03-19 19:37:40 +0500

    add lad 6

--- src/index.js
+++ src/index.js
@@ -52,10 +52,12 @@ class Trasher extends Dog{
     constructor() {
         super('Громила', 5);
     }
+
     modifyTakenDamage(value, fromCard, gameContext, continuation){
             continuation(value-1);
             this.view.signalAbility(() => {this.view.signalDamage(()=>{})})
     }
+
     getDescriptions() {
          let xx = super.getDescriptions();
          xx.unshift("Способность: Получает на 1 урон меньше.")
@@ -72,9 +74,7 @@ class Gatling extends Creature {
     attack(gameContext, continuation) {
         const taskQueue = new TaskQueue();
 
-        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
-
-        for (const oppositeCard of oppositePlayer.table) {
+        for (const oppositeCard of gameContext.oppositePlayer.table) {
             if (oppositeCard)
             {
                 taskQueue.push(onDone => this.view.showAttack(onDone));
@@ -95,22 +95,71 @@ class Gatling extends Creature {
     }
 }
 
+class Lad extends Dog {
+    constructor() {
+        super('Браток', 2);
+    }
+
+    static getInGameCount() { return this.inGameCount || 0; }
+    static setInGameCount(value) { this.inGameCount = value; }
+
+    doAfterComingIntoPlay(gameContext, continuation)
+    {
+        Lad.setInGameCount(Lad.getInGameCount()+1);
+        super.doAfterComingIntoPlay(gameContext, continuation);
+    }
+
+    doBeforeRemoving(continuation)
+    {
+        Lad.setInGameCount(Math.max(Lad.getInGameCount()-1, 0));
+        super.doBeforeRemoving(continuation);
+    }
+
+    static getBonus() {
+        let protection = this.getInGameCount() * (this.getInGameCount() + 1) / 2;
+        let extraDamage = this.getInGameCount() * (this.getInGameCount() + 1) / 2;
+        return {protection, extraDamage};
+    }
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation)
+    {
+        let { protection, extraDamage } = Lad.getBonus();
+        continuation(extraDamage + value);
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation)
+    {
+        let { protection, extraDamage } = Lad.getBonus();
+        continuation(Math.max(value - protection, 0));
+    }
+
+    getDescriptions() {
+        let xx = super.getDescriptions();
+        if (Lad.prototype.hasOwnProperty('modifyDealedDamageToCreature') &&
+                Lad.prototype.hasOwnProperty('modifyTakenDamage'))
+            xx.unshift("Способность: чем их больше, тем они сильнее.")
+        return xx;
+    }
+}
+
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
     new Duck(),
     new Gatling(),
-    new Gatling(),
-    new Gatling(),
     new Duck(),
+    new Duck(),
+    new Gatling(),
+    new Gatling(),
     new Duck(),
 ];
 const banditStartDeck = [
+    new Lad(),
     new Dog(),
+    new Lad(),
     new Trasher(),
-    new Dog(),
-    new Dog(),
-    new Dog(),
-    new Dog(),
+    new Trasher(),
+    new Lad(),
+    new Lad(),
     new Dog(),
 ];
 
