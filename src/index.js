a55fe716eaac04c22ec86d4e36627ab7e726a1dc
magnus5552 <isackov.stepa@yandex.ru>
2024-03-19 17:47:33 +0500

    add constructor

--- src/index.js
+++ src/index.js
@@ -29,7 +29,13 @@ function getCreatureDescription(card) {
 
 
 class Creature extends Card {
-    
+    constructor(name, maxPower, image) {
+        super(name, maxPower, image);
+    }
+
+    getDescriptions() {
+
+    }
 }
 
 
